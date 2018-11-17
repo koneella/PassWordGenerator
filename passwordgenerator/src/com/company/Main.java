490ed025c5100fccb65055e16a8d7df71b0ca439
koneella <janne.olavi3@gmail.com>
2018-11-17 20:35:57 +0200

    passwordgenerator

---
+++ passwordgenerator/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Password pass = new Password();
+        Test test = new Test();
+        Scanner scan = new Scanner(System.in);
+
+       test.generateWindow();
+
+       /*
+        System.out.println("How many characters?");
+        int passwordLength = scan.nextInt();
+
+        System.out.println(pass.generatePassword(passwordLength));
+        */
+
+
+    }
+}
---
+++ passwordgenerator/src/com/company/Password.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.util.Random;
+
+
+public class Password {
+
+    private static final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    private String password = "";
+
+    Random rand = new Random();
+
+
+    public String generatePassword(int n) {
+        for (int i = 0; i < n; i++) {
+            password += chars.charAt(rand.nextInt(chars.length()));
+        }
+        return password;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
